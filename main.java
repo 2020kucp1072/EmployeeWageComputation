eac542177a8d05f3a7d27e503d904d62235bc977
Vemula Dileep <vemuladileep082002@gmail.com>
2024-09-11 11:40:41 +0530

    ADDED functionality to calculate daily wages

--- main.java
+++ main.java
@@ -2,17 +2,29 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+    	
+    	System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+    	
+    	int wagePerHour = 20;
+        int fullDay = 8;
 
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
         if(empCheck == 0)
         {
-            System.out.println("Employee is Present");
+            System.out.print("Employee is Present");
+            System.out.println("");
+            System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
+            System.out.println("");
         }
         else
         {
-            System.out.println("Employee is Absent");
+            System.out.print("Employee is Absent");
+            System.out.println("");
+            System.out.println("The daily wage of Employee is : 0 " );
+            System.out.println("");
         }
-
     }
 }
+
+
+    
