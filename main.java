3346f09273a04fd8d6ebda04348576bda2a60e92
Vemula Dileep <vemuladileep082002@gmail.com>
2024-09-11 23:31:29 +0530

    ADDED functionality for part timr employee

--- main.java
+++ main.java
@@ -7,13 +7,24 @@ public class Main {
     	
     	int wagePerHour = 20;
         int fullDay = 8;
+        int halfDay = 4;
+        
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if(empCheck == 0)
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        if( empCheck == 0 )
         {
             System.out.print("Employee is Present");
             System.out.println("");
             System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
+            System.out.println("");
+        }
+
+        else if ( empCheck == 1 )
+
+        {
+            System.out.print("Employee is Present but working part time ");
+            System.out.println("");
+            System.out.print("The daily wage of Employee is : " + ( wagePerHour * halfDay ));
             System.out.println("");
         }
         else
@@ -23,6 +34,7 @@ public class Main {
             System.out.println("The daily wage of Employee is : 0 " );
             System.out.println("");
         }
+
     }
 }
 
